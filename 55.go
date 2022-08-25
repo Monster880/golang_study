497918de53788ae0b328886c7e9501251c402b7e
liding <539561425@qq.com>
2022-08-25 12:13:20 +0800

    55

---
+++ 55.go
@@ -0,0 +1,16 @@
+func canJump(nums []int) bool {
+	if len(nums) <= 1 {
+		return true
+	}
+	dp := make([]bool, len(nums))
+	dp[0] = true
+	for i := 1; i < len(nums); i++ {
+		for j := i - 1; j >= 0; j-- {
+			if dp[j] && nums[j]+j >= i {
+				dp[i] = true
+				break
+			}
+		}
+	}
+	return dp[len(nums)-1]
+}
